ed20abde7cdfb326d5aaa3b2ef65f3f22356df5d
VarunFuego <45126763+VarunFuego17@users.noreply.github.com>
2022-06-15 01:23:01 +0200

    partial fix joining state

--- aggregation.py
+++ aggregation.py
@@ -7,84 +7,82 @@ from vi.config import Config, dataclass, deserialize
 import numpy as np
 from numpy import random as r
 
+
 @deserialize
 @dataclass
 class AggregationConfig(Config):
     a: float = 2.6
     b: float = 2.2
-    t: int =   10
 
     delta_time: float = 1.0
 
-    def weights(self) -> tuple[float, float, int]:
-        return (self.a, self.b, self.t)
+    def weights(self) -> tuple[float, float]:
+        return (self.a, self.b)
+
 
 class Bee(Agent):
-    state = 0 # 0 -> wandering, 1 -> joining, 2 -> still, 3 -> leaving
-    t_wait = 0
+    state: int = 0 # 0 -> wandering, 1 -> joining, 2 -> still, 3 -> leaving
+    t: int = r.randint(40,50)
+
+
     config: Config
 
-
-    def wandering(self):
-        
+    def wandering(self, in_proximity, a):
         self.pos += self.move
-        
+        uniform_roll = r.uniform()
+        p_join = 0.03 + 0.48 * (1 - np.exp(-a * in_proximity))
+
         if self.on_site():
-            a, _, t = self.config.weights()
-            in_proximity = self.in_proximity_accuracy().count()
-            uniform_roll = r.uniform()
-            
-            p_join = 0.03 + 0.48*(1 - np.exp(-a*in_proximity))
-            
             if p_join > uniform_roll:
+                print('Agent joining')
                 self.state = 1
-        
 
     def join(self):
-        _, _, t = self.config.weights()
-        
-        if self.t_wait == t:    
-            self.t_wait = 0
-            if self.on_site():
-                self.state = 2
-            else:
-                self.state = 0
+        if self.t == 0:
+            print('stop movement')
+            self.t = 30
+            self.state = 2
         else:
+            print(self.t)
             self.pos += self.move
-            self.t_wait += 1
-    
+
+
+
     def still(self):
         self.freeze_movement()
-    
-    def leave(self):
+
+
+    def leave(self, b):
         pass
 
-
     def change_position(self):
-        
+        in_proximity = self.in_proximity_accuracy().count()
+        a, b = self.config.weights()
         self.there_is_no_escape()
-        
+
         if self.state == 0:
-            self.wandering()
+            self.wandering(in_proximity, a)
         if self.state == 1:
+            self.t -= 1
             self.join()
         if self.state == 2:
             self.still()
         if self.state == 3:
-            self.leave()
-            
-            
+            self.leave(b)
+
+
+
 (
     Simulation(
         AggregationConfig(
             image_rotation=True,
-            movement_speed=1.0,
-            radius=15,
-            seed=1,
-            fps_limit =60,
+            movement_speed=1.5,
+            radius=30,
+            seed=2,
+            fps_limit=60,
         )
     )
-        .spawn_site("site_medium.png", 375, 375)
-        .batch_spawn_agents(30, Bee, images=["green.png"])
+        .spawn_site("bubble-full.png", 375, 375)
+        .batch_spawn_agents(50, Bee, images=["green.png"])
         .run()
 )
