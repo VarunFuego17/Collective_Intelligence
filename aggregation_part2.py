2b1b913fc243c510c15ba1a42533b18979d65eee
Lorenzo Gabriele Baracco <lorenzobaracco01@gmail.com>
2022-06-15 22:10:19 +0200

    little T fix

--- aggregation_part2.py
+++ aggregation_part2.py
@@ -16,7 +16,7 @@ class AggregationConfig(Config):
     a: float = 2.6
     b: float = 2.2
     
-    t: int = round(r.normal(30,100)) # t = t_join = t_leave
+    t: int = round(r.normal(70,50)) # t = t_join = t_leave
     d: int = 300 # number of time steps between each p_leave is evaluated
     w: int = 500 # number of time steps it takes to move agent of site if not deciding to join or leaving
 
@@ -53,6 +53,7 @@ class Bee(Agent):
         self.t_step += 1
         
         if self.t_step == t:
+            print(t)
             
             self.t_step = 0
             
