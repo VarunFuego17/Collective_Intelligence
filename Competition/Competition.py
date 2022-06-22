2325a746abc6ae4cfe284d3412e75c2a5da48372
Lorenzo Gabriele Baracco <lorenzobaracco01@gmail.com>
2022-06-22 18:51:33 +0200

    removed unrequested params, bug fix

--- Competition/Competition.py
+++ Competition/Competition.py
@@ -6,20 +6,18 @@ import numpy as np
 from numpy import random as r
 import polars as pl
 import seaborn as sns
-
+from pygame.math import Vector2
 
 @deserialize
 @dataclass
 class CompetitionConfig(Config):
-    fox_energy: int = 600  # initial energy level of a fox
-    hunger: int = 30  # time a fox has to wait before killing a rabbit
-    r_rep: float = 0.2  # rabbit reproduction rate
-    f_rep: float = 0.2  # fox reproduction rate
-    r_death: float = 0.8  # rabbit death rate
-    f_death: float = 0.3  # fox death rate
+    fox_energy: int = 500  # initial energy level of a fox
+    hunger: int = 50  # time a fox has to wait before killing a rabbit
+    r_rep: float = 0.8  # rabbit reproduction rate
+    r_rep_buffer: int = 200  # time steps between each reproduction evaluation
 
-    def weights(self) -> tuple[int, int, float, float, float, float]:
-        return (self.fox_energy, self.hunger, self.r_rep, self.f_rep, self.r_death, self.f_death)
+    def weights(self) -> tuple[int, int, float, int]:
+        return (self.fox_energy, self.hunger, self.r_rep, self.r_rep_buffer)
 
 
 class Fox(Agent):
@@ -36,95 +34,101 @@ class Fox(Agent):
             print(f"FOX: {self.id} died")
             self.kill()
 
-    def consume(self, agent, r_death):
+    def consume(self, hunger):
         """
-        Eat a rabbit given some probability
-        might need some others calls whenever exploring beyond base case.
-        for now it just kills the rabbits.
+        Checks whether there are nearby rabbits to consume and eventually does
         """
-        roll = r.uniform()
-        if r_death > roll:
-            print('KILL')
-            self.energy_t = 0
-            agent.kill()
+        if self.hunger_t == hunger:
+            self.hunger_t = 0
+            prey = (self.in_proximity_accuracy()
+                   .without_distance()
+                   .filter_kind(Rabbit)
+                   .first())
+                
+            if prey is not None:
+                prey.kill()
+                self.energy_t = 0
+                self.reproduction()
         
-    def reproduction(self, f_rep):
+    def reproduction(self):
         """
         Reproduction of foxes gives some probability
         """
-        sample = r.uniform()
-        if sample > f_rep:
+        mate = (self.in_proximity_accuracy()
+               .without_distance()
+               .filter_kind(Fox)
+               .first())
+        
+        if mate is not None:
             self.reproduce()
 
 
     def change_position(self):
+        self.there_is_no_escape()
         self.pos += self.move
         
     def update(self):
-        in_proximity = self.in_proximity_accuracy()
-        count = self.in_proximity_accuracy().count()
-        energy, hunger, _, f_rep, r_death, _ = self.config.weights()  # init params
-        self.there_is_no_escape()
+        energy, hunger, _, _ = self.config.weights()  # init params
 
         self.check_survival(energy)  # kill fox if no energy is left
-        if self.hunger_t == hunger:
-            self.hunger_t = 0
-        for agent, _ in in_proximity:  # check for rabbits within radius x
-            if type(agent) == Rabbit and count == 1 and self.hunger_t == 0:
-                self.consume(agent, r_death)
-            if type(agent) == Fox and self.energy_t != energy:
-                # self.reproduction(f_rep)
-                pass
+        self.consume(hunger)
 
         self.energy_t += 1
         self.hunger_t += 1
 
 class Rabbit(Agent):
+    r_rep_buffer_t = 0
+    
     config: CompetitionConfig
 
-    def wandering(self):
-        """
-        Random Walk
-        """
-
-        self.pos = self.pos + self.move
-        return self.pos
-
-    def reproduction(self, r_rep):
+    def reproduction(self, r_rep, r_rep_buffer):
         """
         Reproduction of Rabbits
         """
-        sample = r.uniform()
-        if sample > r_rep:
-            self.reproduce()
+        if self.r_rep_buffer_t == r_rep_buffer:
+            mate = (self.in_proximity_accuracy()
+                    .without_distance()
+                    .filter_kind(Rabbit)
+                    .first())
+        
+            if mate is not None:
+                roll = r.uniform()
+                if roll > r_rep:
+                    new_x = r.uniform()
+                    new_y = r.uniform()
+                    
+                    self.reproduce()
+                    self.move = Vector2(new_x, new_y)
+                    
+                    self.r_rep_buffer_t = 0
+        else:
+            self.r_rep_buffer_t += 1
+        
 
     def change_position(self):
-        in_proximity = self.in_proximity_accuracy()
-        count = self.in_proximity_accuracy().count()
-        _, _, r_rep, _, _, _ = self.config.weights()  # init params
-
-        for agent, _ in in_proximity: # reproduction of rabbits
-            if count == 1 and type(agent) == Rabbit:
-                pass
-                #self.reproduction(r_rep)
 
         self.there_is_no_escape()
-        self.wandering()
+        self.pos += self.move
+        
+        
+    def update(self):
+        _, _, r_rep, r_rep_buffer = self.config.weights()  # init params
+        self.reproduction(r_rep, r_rep_buffer)
 
 
 (
     Simulation(
         CompetitionConfig(
             image_rotation=True,
-            movement_speed=1.0,
+            movement_speed=1.5,
             radius=25,
             seed=30,
             fps_limit=60,
-            window=Window.square(1000),
+            window=Window.square(700),
         )
     )
-        .batch_spawn_agents(100, Fox, images=["red.png"])
-        .batch_spawn_agents(100, Rabbit, images=["green.png"])
+        .batch_spawn_agents(100, Fox, images=["Fox.png"])
+        .batch_spawn_agents(100, Rabbit, images=["Rabbit.png"])
         .run()
     # .snapshots.groupby(["frame","site_id"])
     # .agg(pl.count("id").alias("agent"))
