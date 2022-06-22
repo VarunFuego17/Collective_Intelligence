40dcb8a66a75dc800340798f9f75a40db7011e56
VarunFuego <45126763+VarunFuego17@users.noreply.github.com>
2022-06-22 22:26:56 +0200

    Added fox behavior + offspring range for reproduction

--- Competition/Competition.py
+++ Competition/Competition.py
@@ -6,20 +6,21 @@ import numpy as np
 from numpy import random as r
 import polars as pl
 import seaborn as sns
+from pygame.math import Vector2
 
 
 @deserialize
 @dataclass
 class CompetitionConfig(Config):
-    fox_energy: int = 600  # initial energy level of a fox
-    hunger: int = 30  # time a fox has to wait before killing a rabbit
-    r_rep: float = 0.2  # rabbit reproduction rate
-    f_rep: float = 0.2  # fox reproduction rate
-    r_death: float = 0.8  # rabbit death rate
-    f_death: float = 0.3  # fox death rate
+    fox_energy: int = 300  # initial energy level of a fox
+    hunger: int = 50  # time a fox has to wait before killing a rabbit
+    r_rep: float = 0.8  # rabbit reproduction rate
+    r_rep_buffer: int = 300  # time steps between each reproduction evaluation
+    offspring: int = 3 # range of offspring produced per reproduction
+    align: bool = False # Turn alignment on or off
 
-    def weights(self) -> tuple[int, int, float, float, float, float]:
-        return (self.fox_energy, self.hunger, self.r_rep, self.f_rep, self.r_death, self.f_death)
+    def weights(self) -> tuple[int, int, float, int, int, bool]:
+        return (self.fox_energy, self.hunger, self.r_rep, self.r_rep_buffer, self.offspring, self.align)
 
 
 class Fox(Agent):
@@ -28,10 +29,26 @@ class Fox(Agent):
 
     config: CompetitionConfig
 
-    def wandering(self):
-        "Random walk"
-        self.pos = self.pos + self.move
-        return self.pos
+
+    def alignment(self):
+        vectors = np.array([[self.move[0]],[self.move[1]]])
+        count = self.in_proximity_accuracy().count()
+        fox =   (self.in_proximity_accuracy()
+                 .filter_kind(Fox)
+        )
+
+        if fox.count() == 3:
+            scalar = 1 / (count)
+            for agent, distance in fox:
+
+               vectors = np.append(vectors, np.array([[agent.move[0]],[agent.move[1]]]), axis=1)
+
+            new_move = scalar * np.sum(vectors, axis=1)
+            new_move = Vector2(new_move[0], new_move[1]).normalize()
+            return new_move - self.move
+        else:
+            return self.move
+
 
     def check_survival(self, energy):
         """
@@ -41,81 +58,98 @@ class Fox(Agent):
             print(f"FOX: {self.id} died")
             self.kill()
 
-    def consume(self, agent, r_death):
+    def consume(self, hunger, offspring):
         """
-        Eat a rabbit given some probability
-        might need some others calls whenever exploring beyond base case.
-        for now it just kills the rabbits.
+        Checks whether there are nearby rabbits to consume and eventually does
         """
-        sample = r.uniform()
-        if r_death > sample:
-            print('KILL')
-            self.energy_t = 0
-            agent.kill()
-        
-    def reproduction(self, f_rep):
+        if self.hunger_t == hunger:
+            self.hunger_t = 0
+            prey = (self.in_proximity_accuracy()
+                   .without_distance()
+                   .filter_kind(Rabbit)
+                   .first())
+
+            if prey is not None:
+                prey.kill()
+                self.energy_t = 0
+                self.reproduction(offspring)
+
+    def reproduction(self, offspring):
         """
-        Reproduction of foxes gives some probability
+        Reproduction of foxes
+
+        mate = (self.in_proximity_accuracy()
+               .without_distance()
+               .filter_kind(Fox)
+               .first())
+
+        if mate is not None:
         """
-        sample = r.uniform()
-        if sample > f_rep:
+        for i in range(r.randint(1,offspring)):
+            new_x = r.uniform()
+            new_y = r.uniform()
             self.reproduce()
+            self.move = Vector2(new_x, new_y)
 
 
 
     def change_position(self):
-        in_proximity = self.in_proximity_accuracy()
-        count = self.in_proximity_accuracy().count()
-        energy, hunger, _, f_rep, r_death, _ = self.config.weights()  # init params
+        _, _, _, _, _, align = self.config.weights()
         self.there_is_no_escape()
+        if align:
+            self.pos += self.alignment()
+        else:
+            self.pos += self.move
+
+    def update(self):
+        energy, hunger, _, _, offspring, _ = self.config.weights()  # init params
 
         self.check_survival(energy)  # kill fox if no energy is left
-        if self.hunger_t == hunger:
-            self.hunger_t = 0
-        for agent, _ in in_proximity:  # check for rabbits within radius x
-            if type(agent) == Rabbit and count == 1 and self.hunger_t == 0:
-                self.consume(agent, r_death)
-            if type(agent) == Fox and self.energy_t != energy:
-                # self.reproduction(f_rep)
-                pass
+        self.consume(hunger , offspring)
 
         self.energy_t += 1
         self.hunger_t += 1
 
-        self.wandering()  # updating movement of fox
-
-
 class Rabbit(Agent):
+    r_rep_buffer_t = 0
+
     config: CompetitionConfig
 
-    def wandering(self):
-        """
-        Random Walk
-        """
-
-        self.pos = self.pos + self.move
-        return self.pos
-
-    def reproduction(self, r_rep):
+    def reproduction(self, r_rep, r_rep_buffer, offspring):
         """
         Reproduction of Rabbits
         """
-        sample = r.uniform()
-        if sample > r_rep:
-            self.reproduce()
+        if self.r_rep_buffer_t == r_rep_buffer:
+            mate = (self.in_proximity_accuracy()
+                    .without_distance()
+                    .filter_kind(Rabbit)
+                    .first())
+
+            if mate is not None:
+                roll = r.uniform()
+                if roll > r_rep:
+
+                    for i in range(r.randint(1, offspring)):
+                        new_x = r.uniform()
+                        new_y = r.uniform()
+
+                        self.reproduce()
+                        self.move = Vector2(new_x, new_y)
+
+                    self.r_rep_buffer_t = 0
+        else:
+            self.r_rep_buffer_t += 1
+
 
     def change_position(self):
-        in_proximity = self.in_proximity_accuracy()
-        count = self.in_proximity_accuracy().count()
-        _, _, r_rep, _, _, _ = self.config.weights()  # init params
-
-        for agent, _ in in_proximity: # reproduction of rabbits
-            if count == 1 and type(agent) == Rabbit:
-                pass
-                #self.reproduction(r_rep)
 
         self.there_is_no_escape()
-        self.wandering()
+        self.pos += self.move
+
+
+    def update(self):
+        _, _, r_rep, r_rep_buffer, offspring, _ = self.config.weights()  # init params
+        self.reproduction(r_rep, r_rep_buffer, offspring)
 
 
 (
@@ -123,17 +157,17 @@ class Rabbit(Agent):
         CompetitionConfig(
             image_rotation=True,
             movement_speed=1.0,
-            radius=25,
-            seed=30,
+            radius=10,
+            seed=12,
             fps_limit=60,
             window=Window.square(1000),
         )
     )
-        .batch_spawn_agents(20, Fox, images=["red.png"])
-        .batch_spawn_agents(40, Rabbit, images=["green.png"])
+        .batch_spawn_agents(100, Fox, images=["red.png"])
+        .batch_spawn_agents(100, Rabbit, images=["green.png"])
         .run()
     # .snapshots.groupby(["frame","site_id"])
-    # .agg(pl.count("id").alias("agent"))
+    # .agg(pl.count("id").alias("agenta"))
     # .sort(["frame", "site_id"])
 
 )
