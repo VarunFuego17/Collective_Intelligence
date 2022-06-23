88b5eb6f870df85598ac5c574afd5078792ec316
Lorenzo Gabriele Baracco <lorenzobaracco01@gmail.com>
2022-06-23 10:43:02 +0200

    minor adjustments

--- Competition/Competition.py
+++ Competition/Competition.py
@@ -15,9 +15,10 @@ class CompetitionConfig(Config):
     hunger: int = 50  # time a fox has to wait before killing a rabbit
     r_rep: float = 0.8  # rabbit reproduction rate
     r_rep_buffer: int = 200  # time steps between each reproduction evaluation
+    align: bool = True # Turn alignment on or off
 
-    def weights(self) -> tuple[int, int, float, int]:
-        return (self.fox_energy, self.hunger, self.r_rep, self.r_rep_buffer)
+    def weights(self) -> tuple[int, int, float, int, bool]:
+        return (self.fox_energy, self.hunger, self.r_rep, self.r_rep_buffer, self.align)
 
 
 class Fox(Agent):
@@ -25,6 +26,30 @@ class Fox(Agent):
     hunger_t: int = 0
 
     config: CompetitionConfig
+    
+    def alignment(self):
+        """
+        When turned on, alignment will steer the direction
+        of x Foxes within radius r to the average direction
+        of the x Foxes
+        """
+        vectors = np.array([[self.move[0]],[self.move[1]]])
+        count = self.in_proximity_accuracy().count()
+        fox =   (self.in_proximity_accuracy()
+                 .filter_kind(Fox)
+        )
+
+        if fox.count() == self.alignment_max:
+            scalar = 1 / (count)
+            for agent, distance in fox:
+
+               vectors = np.append(vectors, np.array([[agent.move[0]],[agent.move[1]]]), axis=1)
+
+            new_move = scalar * np.sum(vectors, axis=1)
+            new_move = Vector2(new_move[0], new_move[1]).normalize()
+            return new_move - self.move
+        else:
+            return self.move
 
     def check_survival(self, energy):
         """
@@ -65,10 +90,16 @@ class Fox(Agent):
 
     def change_position(self):
         self.there_is_no_escape()
-        self.pos += self.move
+        
+        _, _, _, _, align = self.config.weights()
+        self.there_is_no_escape()
+        if align:
+            self.pos += self.alignment()
+        else:
+            self.pos += self.move
         
     def update(self):
-        energy, hunger, _, _ = self.config.weights()  # init params
+        energy, hunger, _, _, _ = self.config.weights()  # init params
 
         self.check_survival(energy)  # kill fox if no energy is left
         self.consume(hunger)
@@ -112,7 +143,7 @@ class Rabbit(Agent):
         
         
     def update(self):
-        _, _, r_rep, r_rep_buffer = self.config.weights()  # init params
+        _, _, r_rep, r_rep_buffer, _ = self.config.weights()  # init params
         self.reproduction(r_rep, r_rep_buffer)
 
 
